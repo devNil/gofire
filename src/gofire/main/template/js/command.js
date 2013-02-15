275ac406703447e4b71e4426a4a2e2f981fc2216
Yannik Dällenbach <yannikdbach@gmail.com>
2013-02-16 00:45:05 +0100

    constants are in seperate file

---
+++ src/gofire/main/template/js/command.js
@@ -0,0 +1,8 @@
+const REGISTER = 0 //Register a user
+//Text-Messaging Commands
+const BMESSAGE = 1 //Broadcast-Message
+const UMESSAGE = 2 //Unicast-Message
+const MMESSAGE = 3 //Multicast-Message
+	
+const BLOGIN = 4 //Broadcast-Login
+const BLOGOUT = 5 //Broadcast-Logout
