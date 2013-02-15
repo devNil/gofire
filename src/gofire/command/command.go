16775d1aeedcc52a15696693127cebd9c3223fe8
Yannik Dällenbach <yannikdbach@gmail.com>
2013-02-15 14:52:46 +0100

    new command package

---
+++ src/gofire/command/command.go
@@ -0,0 +1,17 @@
+// Package command provides commands for querying both, server and client.
+package command
+
+type CommandType int
+
+const (
+	REGISTER CommandType = iota //Register a user
+	MESSAGE                     //Message in Value
+	BLOGIN
+	BLOGOUT
+)
+
+//Every Command has a type and a value for sending arbitrary data.
+type Command struct {
+	Type  CommandType
+	Value []byte
+}
