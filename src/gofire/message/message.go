3c4fd66f4238a7a5059abfa57bbef7eec4173ed5
Yannik Dällenbach <yannikdbach@gmail.com>
2013-02-16 00:59:53 +0100

    added Message struct

--- src/gofire/message/message.go
+++ src/gofire/message/message.go
@@ -1,3 +1,9 @@
-//Contains all message related stuff
+//will all message related stuff
 package message
 
+import "gofire/user"
+
+type Message struct {
+	User *user.User //User who sended the message
+	Msg []byte //For arbitrary data
+}
