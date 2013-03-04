e811b25908dc47613a5726f3c04690784190a2e6
Yannik Dällenbach <yannikdbach@gmail.com>
2013-03-04 15:58:05 +0100

    New Methods, whole package is now like the net/http package

--- src/gofire/server/server.go
+++ src/gofire/server/server.go
@@ -12,35 +12,54 @@ type FireServer struct{
 	RegisteredChatRooms []string
 }
 
-func (fs *FireServer)Init(){
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){fs.MainHandler(w, r)})
-	http.HandleFunc(CHAT, func(w http.ResponseWriter, r *http.Request){fs.ChatRoomHandler(w, r)})
-	err := http.ListenAndServe(fs.Addr, nil)
-	if err != nil {
-		panic("ListenAndServe: " + err.Error())
-	}
+//a fireserver instance
+var fireServer = new(FireServer)
+var  restCommands = make(map[string]string)
+
+func init(){
+	AddRestCommand("/api", MainHandler, "Get all commands")
+	AddRestCommand("/api/c", ChatRoomHandler, "Get all chatrooms")
+
+	initServer()
 }
 
-func (fs *FireServer)MainHandler(w http.ResponseWriter, r *http.Request){
+//adds a rest command 
+func AddRestCommand(pattern string,handler func(http.ResponseWriter, *http.Request),desc string){
+	restCommands[pattern] = desc
+	http.HandleFunc(pattern, handler)
+}
+
+func ListenAndServe(addr string)error{
+	fireServer.Addr = addr
+	err := http.ListenAndServe(addr, nil)
+	return err
+}
+
+func initServer(){
+	http.HandleFunc("/", MainHandler)
+	http.HandleFunc(CHAT, ChatRoomHandler)
+}
+
+func MainHandler(w http.ResponseWriter, r *http.Request){
 	if r.URL.Path[len(r.URL.Path)-3:] == "api" {
-		fs.apiHandler(w, r)
+		apiHandler(w, r)
 	}
 }
 
 //Chat-rounting
 const CHAT = "/c/"
 //ChatRoomHandler
-func (fs *FireServer)ChatRoomHandler(w http.ResponseWriter, r *http.Request){
+func ChatRoomHandler(w http.ResponseWriter, r *http.Request){
 	chatName := r.URL.Path[len(CHAT):]
 	if len(chatName) == 0{
-		fs.getAllChatrooms(w, r)
+		getAllChatrooms(w, r)
 	}else{
 		w.Write([]byte(chatName))
 	}
 }
 
-func (fs *FireServer)getAllChatrooms(w http.ResponseWriter, r *http.Request){
-	json,err := json.Marshal(fs.RegisteredChatRooms)
+func getAllChatrooms(w http.ResponseWriter, r *http.Request){
+	json,err := json.Marshal(fireServer.RegisteredChatRooms)
 	if err == nil{
 		w.Write(json)
 	}else{
@@ -49,8 +68,8 @@ func (fs *FireServer)getAllChatrooms(w http.ResponseWriter, r *http.Request){
 }
 
 //Api Hanlder, Handles /api calls on server
-func (fs *FireServer)apiHandler(w http.ResponseWriter, r *http.Request){
-	json, err := json.Marshal(fs)
+func apiHandler(w http.ResponseWriter, r *http.Request){
+	json, err := json.Marshal(restCommands)
 	if err != nil{
 		w.Write([]byte("Fail"))
 	}else{
