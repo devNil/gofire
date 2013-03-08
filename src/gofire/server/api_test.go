af24585a6ab12d4ee744065f185c8900218a189d
Yannik Dällenbach <yannikdbach@gmail.com>
2013-03-08 16:49:22 +0100

    new testcase and comments

--- src/gofire/server/api_test.go
+++ src/gofire/server/api_test.go
@@ -1,19 +1,21 @@
+//Use this test with a started fireserver
+//TODO: Inegrate fireserver here
 package server
 
-import(
-	"testing"
-	"net/http"
+import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"encoding/json"
+	"net/http"
+	"testing"
 )
 
 const ADRESS = "http://localhost"
 
 //First Api Test for /api
-func TestOveview(t *testing.T){
-	resp, err := http.Get(ADRESS+":8080/api")
-	if err != nil{
+func TestOveview(t *testing.T) {
+	resp, err := http.Get(ADRESS + ":8080/api")
+	if err != nil {
 		t.Log(err)
 		t.Fail()
 	}
@@ -21,17 +23,33 @@ func TestOveview(t *testing.T){
 	body, err := ioutil.ReadAll(resp.Body)
 
 	json, er := json.Marshal(restCommands)
-	
 	if er != nil {
 		t.Log(er)
 		t.Fail()
 	}
 
-	if string(json) != string(body){ 
+	if string(json) != string(body) {
 		t.Log("not the same")
 		t.Fail()
 	}
 
+	fmt.Println(string(body))
+}
+//Test for /api/c
+func TestChatOverview(t *testing.T) {
+	resp, err := http.Get(ADRESS + ":8080/api/c")
+	if err != nil {
+		t.Log(err)
+		t.Fail()
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
 
 	fmt.Println(string(body))
+
+	if string(body) != string(http.StatusNotFound) {
+		t.Log("Not the same")
+		t.Fail()
+	}
+
 }
