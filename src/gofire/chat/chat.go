c35448a510404924e2f1bc32b33c7ffc49ac5fa8
Yannik Dällenbach <yannikdbach@gmail.com>
2013-03-08 17:05:17 +0100

    added user package dependency and *user slice  for all registered user

--- src/gofire/chat/chat.go
+++ src/gofire/chat/chat.go
@@ -1,6 +1,11 @@
 //This package provides all chatroom-functionality
 package chat
 
+import(
+	"gofire/user"
+)
+
 type ChatRoom struct{
-	Name string
+	Name string //The name of the chatroom, also the id of a chatroom
+	User *[]user.User //List of all user in this chatroom
 }
