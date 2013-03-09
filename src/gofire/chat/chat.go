c09b4fbe5eaaec2a85a7d6feae4bcb549ee956e3
Yannik Dällenbach <yannikdbach@gmail.com>
2013-03-09 13:32:40 +0100

    added connection struct and ported chatroom struct

--- src/gofire/chat/chat.go
+++ src/gofire/chat/chat.go
@@ -3,9 +3,19 @@ package chat
 
 import(
 	"gofire/user"
+	"gofire/message"
+	"code.google.com/p/go.net/websocket"
 )
+
+type Connection struct{
+	User *user.User //User in connection
+	conn *websocket.Conn
+	send chan *command.Command
+}
 
 type ChatRoom struct{
 	Name string //The name of the chatroom, also the id of a chatroom
-	User *[]user.User //List of all user in this chatroom
+	register chan *Connection
+	unregister chan *Connection
+	registeredConnections map[*Connection]bool
 }
