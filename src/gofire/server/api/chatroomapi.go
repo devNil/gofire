e17fdd092632787a9142649124e334da2ca64259
Yannik Dällenbach <yannikdbach@gmail.com>
2013-03-09 13:24:59 +0100

    /api/c api methods

--- src/gofire/server/api/chatroomapi.go
+++ src/gofire/server/api/chatroomapi.go
@@ -5,3 +5,26 @@ import(
 	"gofire/server"
 	"net/http"
 )
+
+const(
+	POST = "POST"
+	GET = "GET"
+)
+
+//differs POST or GET /api/c
+func ChatRoomHandler(fireServer *server.FireServer, w http.ResponseWriter, r *http.Request){
+	if r.Method == POST{
+		postChatRoomHandler(fireServer, w, r)
+	}
+	if r.Method == GET{
+		getChatRoomHandler(fireServer, w, r)
+	}
+}
+
+func postChatRoomHandler(fireServer *server.FireServer, w http.ResponseWriter, r *http.Request){
+
+}
+
+func getChatRoomHandler(fireServer *server.FireServer, w http.ResponseWriter, r *http.Request){
+
+}
