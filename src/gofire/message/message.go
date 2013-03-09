683d651270e84fdf6fda1bc9a3c4885a42fdcaa5
Yannik Dällenbach <yannikdbach@gmail.com>
2013-03-09 13:44:55 +0100

    messagetypes added

--- src/gofire/message/message.go
+++ src/gofire/message/message.go
@@ -3,6 +3,12 @@ package message
 
 import "gofire/user"
 
+type MessageType int
+
+const(
+	LOGIN MessageType = iota //User logged in message
+)
+
 type Message struct {
 	User *user.User //User who sended the message
 	Msg  []byte     //For arbitrary data
