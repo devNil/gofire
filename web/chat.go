78188cbe1ad76f0cc217ab5b2d49d60d852cbd1f
Yannik Dällenbach <yannikdbach@gmail.com>
2013-04-22 21:48:15 +0200

    added basic chat handler

---
+++ web/chat.go
@@ -0,0 +1,14 @@
+package web
+
+import(
+	"net/http"
+)
+
+func ChatHandler(w http.ResponseWriter, r *http.Request){
+	token := CheckSession(r)
+
+	if token == ""{
+		http.Redirect(w, r, "/", http.StatusOk)
+		return
+	}
+}
