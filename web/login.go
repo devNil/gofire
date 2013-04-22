22fd8aaeb78244f084ccd10d51411e5157e73fab
Yannik Dällenbach <yannikdbach@gmail.com>
2013-04-22 21:23:29 +0200

    added login semantics

---
+++ web/login.go
@@ -0,0 +1,26 @@
+package web
+
+import(
+	"net/http"
+	db "database/gofire"
+	"time"
+)
+
+func LoginHandler(w http.ResponseWriter, r *http.Request){
+	r.ParseForm()
+	username := r.FormValue("user")
+	password := r.FormValue("pw")
+
+	token := db.IsUserPasswordValid(username, password)
+
+	if token == ""{
+		http.Redirect(w, r, "/", http.StatusTextOk)
+		return
+	}
+
+	d := time.Now.Add(356*24*time.Hour)
+	cookie := &http.Cookie{Name:GofireSession, Value:token, Expires:d, HttpOnly:true}
+	http.SetCookie(w, cookie)
+	http.Redirect(w, r, "/chat", http.StatusFound)
+	return
+}
