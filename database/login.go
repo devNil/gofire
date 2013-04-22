fd08a43caccd46fefe69e97a92aefe528ead0e94
Yannik Dällenbach <yannikdbach@gmail.com>
2013-04-22 20:54:55 +0200

    added function for validating sessionwq

---
+++ database/login.go
@@ -0,0 +1,28 @@
+package database
+
+import(
+	"database/sql"
+	_ "github.com/bmizerany/pq"
+	"log"
+)
+
+const qIsSessionValid = "select count(*) from gf_users where session=$1"
+
+func IsSessionValid(token string)bool{
+	conn := Open()
+	defer conn.Close()
+
+	var count int64
+
+	row := db.QueryRow(qIsSessionValid, token)
+	
+	err := row.Scan(&count)
+
+	if err != nil{
+		log.Println(err)
+		return false
+	}
+
+	return count == 1
+
+}
