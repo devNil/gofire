1826cd313caf1e22669f7392aaea2ed438365ed8
Yannik Dällenbach <yannikdbach@gmail.com>
2013-04-22 20:01:54 +0200

    added setup file for local configs

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#. ./setup for running
+#PORT for webserver
+export PORT=8080
+#Template-Path
+export TEMPLATE=web/
+#database user
+export DUSER=$USER
+#database user password
+export DPW=
+#database
+export DDB=gofire
+#databse hostaddress
+export DHOST=
