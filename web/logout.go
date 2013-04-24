75ca96db33a5f4e220d8f1a634289086b3577fe4
neo <noe.thalheim@gmail.com>
2013-04-24 13:49:05 +0200

    close #26

--- web/logout.go
+++ web/logout.go
@@ -2,16 +2,14 @@ package web
 
 import (
 	"net/http"
-//	"time"
+	"log"
 )
 func LogoutHandler(w http.ResponseWriter, r *http.Request){
     if cookie, err :=r.Cookie(GofireSession); err != nil{
-		return
+		log.Println(err)
 	} else {
 		cookie.MaxAge = -1
-		//cookie.Expires = time.Now()
 		http.SetCookie(w, cookie)
-		http.Redirect(w, r, "/", http.StatusFound)
-		return
+	    http.Redirect(w, r, "/", http.StatusFound)
 	}
 }
