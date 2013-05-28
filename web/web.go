b9ea5d2649aadd036ccf46f8cd61a5194d3843c8
Yannik Dällenbach <yannikdbach@gmail.com>
2013-05-28 20:17:04 +0200

    change to new global templates

--- web/web.go
+++ web/web.go
@@ -10,18 +10,14 @@ import (
 	db "gofire/database"
 )
 
-var index *template.Template
-var chat *template.Template
+var templates *template.Template
 
 func init() {
 	tdir := os.Getenv("TEMPLATE")
 	log.Printf("Template Directory: %s\n", tdir)
 
-	indexPath := fmt.Sprintf("%s%s", tdir, "index.html")
-	index = template.Must(template.ParseFiles(indexPath))
+    templates = template.Must(template.ParseGlob(tdir))
 
-	chatPath := fmt.Sprintf("%s%s", tdir, "chat.html")
-	chat = template.Must(template.ParseFiles(chatPath))
 }
 
 const GofireSession = "gSession"
