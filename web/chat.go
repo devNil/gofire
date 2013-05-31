be67349b85a13030c220e906e081d6a8ec97f119
Yannik Dällenbach <yannikdbach@gmail.com>
2013-05-31 22:11:16 +0200

    check if session is new or not

--- web/chat.go
+++ web/chat.go
@@ -5,12 +5,13 @@ import(
 )
 
 func ChatHandler(w http.ResponseWriter, r *http.Request){
-	token := CheckSession(r)
 
-	if token == ""{
-		http.Redirect(w, r, "/", http.StatusFound)
-		return
-	}
+    session, err := store.Get(r, cookieName)
+
+    if err != nil || session.IsNew {
+        http.Redirect(w,r, "/", http.StatusFound)
+        return
+    }
 
 	w.Header().Set("content-type", "text/html")
     templates.ExecuteTemplate(w,"chat", r.Host)
