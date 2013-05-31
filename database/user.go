d44cf579c6f393544d961f26ebcbb333ff5817a7
Yannik Dällenbach <yannikdbach@gmail.com>
2013-05-31 21:55:06 +0200

    add get for id

--- database/user.go
+++ database/user.go
@@ -10,6 +10,7 @@ type User struct{
 
 const qGetUser = "select id, login, mod, session from gf_user where id = $1"
 
+//retrieve userobject with id
 func GetUser(id int64)(*User, error){
     conn := Open()
     defer conn.Close()
@@ -25,4 +26,18 @@ func GetUser(id int64)(*User, error){
     return &user
 }
 
+const qGetUserId = "select id from gf_user where login=$1 and pw=$2"
+
+//get user id with username and password combination
+func GetUserId(username, password)(int64, error){
+    conn := Open()
+    defer conn
+
+    var id int64
+
+    row := conn.QueryRow(qGetUserId, username, sha512(password))
+
+    err := row.Scan(&id)
+
+    return id, err
 }
