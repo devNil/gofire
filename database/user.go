17fd4ae1a47d4c175c74aaecd232045d203b6212
Yannik Dällenbach <yannikdbach@gmail.com>
2013-05-31 21:58:42 +0200

    fix syntax errors

--- database/user.go
+++ database/user.go
@@ -15,23 +15,23 @@ func GetUser(id int64)(*User, error){
     conn := Open()
     defer conn.Close()
     
-    user = new(User)
+    user := new(User)
     var mod int64
 
     row := conn.QueryRow(qGetUser, id)
-    err := row.Scan(&user.Id, &user.Login, &mod, &user.Session)
+    err := row.Scan(user.Id, user.Login, &mod, user.Session)
 
-    &user.Admin = mod == 1
+    user.Admin = mod == 1
 
-    return &user
+    return user, err
 }
 
 const qGetUserId = "select id from gf_user where login=$1 and pw=$2"
 
 //get user id with username and password combination
-func GetUserId(username, password)(int64, error){
+func GetUserId(username, password string)(int64, error){
     conn := Open()
-    defer conn
+    defer conn.Close()
 
     var id int64
 
