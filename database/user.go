651330c23e168f742252a9842f4aaf48c56dec56
Yannik Dällenbach <yannikdbach@gmail.com>
2013-05-31 21:15:12 +0200

    add user struct and new method

--- database/user.go
+++ database/user.go
@@ -1,27 +1,28 @@
 package database
 
-import(
-	"log"
-)
+//User object retrieved from database
+type User struct{
+    Id int64
+    Login string
+    Admin bool
+    Session string
+}
 
-const qGetUser = "select login from gf_user where session = $1"
+const qGetUser = "select id, login, mod, session from gf_user where id = $1"
 
-func GetUser(token string)string{
-	conn :=Open()
-	defer conn.Close()
+func GetUser(id int64)(*User, error){
+    conn := Open()
+    defer conn.Close()
+    
+    user = new(User)
+    var mod int64
 
-	var user string
+    row := conn.QueryRow(qGetUser, id)
+    err := row.Scan(&user.Id, &user.Login, &mod, &user.Session)
 
-	row := conn.QueryRow(qGetUser, token)
+    &user.Admin = mod == 1
 
-	err := row.Scan(&user)
-
-	if err != nil{
-		log.Println(err)
-		return ""
-	}
-
-	return user
-
+    return &user
+}
 
 }
